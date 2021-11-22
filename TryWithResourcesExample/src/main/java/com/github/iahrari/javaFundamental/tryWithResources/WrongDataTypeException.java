32bf23842a7fc2a80d7636c76a209e3d5f240135
Iman Ahrari <iahrari@pm.me>
2021-11-22 20:35:19 +0330

    Added message in constructor, and refactored code style

--- TryWithResourcesExample/src/main/java/com/github/iahrari/javaFundamental/tryWithResources/WrongDataTypeException.java
+++ TryWithResourcesExample/src/main/java/com/github/iahrari/javaFundamental/tryWithResources/WrongDataTypeException.java
@@ -1,13 +1,10 @@
+package com.github.iahrari.javaFundamental.tryWithResources;
+
 public class WrongDataTypeException extends Exception{
     private final String data;
     private final int lineNumber;
 
-    public WrongDataTypeException(
-        String data, 
-        String message, 
-        int lineNumber, 
-        Throwable cause
-    ) {
+    public WrongDataTypeException(String data, int lineNumber, String message,Throwable cause) {
         super(message, cause);
         this.data = data;
         this.lineNumber = lineNumber;
